b664836642779275c8d3f83fc6183e380fdd988c
Cristofer Binimelis <cristoferbinimelis@gmail.com>
2024-10-19 14:51:39 -0300

    Agregar archivos de proyecto.

---
+++ 01_FizzBuzz/Program.cs
@@ -0,0 +1,31 @@
+﻿/*
+ * Escribe un programa que muestre por consola (con un print) los
+ * números de 1 a 100 (ambos incluidos y con un salto de línea entre
+ * cada impresión), sustituyendo los siguientes:
+ * - Múltiplos de 3 por la palabra "fizz".
+ * - Múltiplos de 5 por la palabra "buzz".
+ * - Múltiplos de 3 y de 5 a la vez por la palabra "fizzbuzz".
+ */
+
+WriteLine("###  FIZZBUZZ    ###");
+WriteLine("Conteo de números del 1 al 100");
+
+for (int i = 1; i < 101; i++)
+{
+    if (i % 5 == 0 & i % 3 == 0)
+    {
+        WriteLine(i + " - FizzBuzz");
+    }
+    else if (i % 3 == 0)
+    {
+        WriteLine(i + " - Fizz");
+    }
+    else if (i % 5 == 0)
+    {
+        WriteLine(i + " - Buzz");
+    }
+    else
+    {
+        WriteLine(i);
+    }
+}
---
+++ 02_Anagrama/Program.cs
@@ -0,0 +1,38 @@
+﻿/*
+ * Escribe una función que reciba dos palabras (String) y retorne
+ * verdadero o falso (Bool) según sean o no anagramas.
+ * - Un Anagrama consiste en formar una palabra reordenando TODAS
+ *   las letras de otra palabra inicial.
+ * - NO hace falta comprobar que ambas palabras existan.
+ * - Dos palabras exactamente iguales no son anagrama.
+ */
+
+WriteLine("###  ¿ES UN ANAGRAMA?    ###");
+
+WriteLine("Ingresa la 1ª palabra: ");
+string? primeraPalabra = ReadLine();
+
+WriteLine("Ingresa la 2ª palabra: ");
+string? segundaPalabra = ReadLine();
+
+string mensaje = "";
+
+mensaje = ComprobarAnagrama(primeraPalabra, segundaPalabra) ? "Es un anagrama" : "Es una palabra";
+
+WriteLine(mensaje);
+bool ComprobarAnagrama(string p1, string p2)
+{
+    bool decision = true;
+    char[] pArray1= p1.ToCharArray();
+    char[] pArray2= p2.ToCharArray();
+
+    Array.Sort(pArray1);
+    Array.Sort(pArray2);
+
+    for ( int i = 0; i < pArray1.Length; i++)
+    {
+        if (pArray1[i] !=  pArray2[i])
+            decision = false;
+    }
+    return decision;
+}
